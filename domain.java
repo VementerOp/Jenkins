bd32346e36a4c44e88e3cb27927e0a965fcadf0f
Atharva Sadhye <113588341+VementerOp@users.noreply.github.com>
2026-03-05 14:25:44 +0530

    Update domain.java

--- domain.java
+++ domain.java
@@ -2,5 +2,6 @@ public class domain{
   public static void main(String args[]){
 	System.out.println("This is the jenkins practical");
 	System.out.println("change");
+	System.out.println("change 2");
 }
 }
