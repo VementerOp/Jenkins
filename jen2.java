a69e76868b90f95acce90c653af42a99c25bc08a
Atharva Sadhye <113588341+VementerOp@users.noreply.github.com>
2026-03-08 21:25:33 +0530

    Update jen2.java

--- jen2.java
+++ jen2.java
@@ -2,6 +2,7 @@ public class jen2{
   public static void main(String args[]){
 	System.out.println("This is the jenkins practical");
 	System.out.println("Hello Atharva!!");
-  System.out.println("This is the Java file for devops question 8");
+    System.out.println("This is the Java file for devops question 8");
+	System.out.println("Updated File");
   }
 }
