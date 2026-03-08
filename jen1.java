4e4576209bf239c98cc72da5a679eda10674983f
Atharva Sadhye <113588341+VementerOp@users.noreply.github.com>
2026-03-08 18:56:04 +0530

    Update jen1.java

--- jen1.java
+++ jen1.java
@@ -2,5 +2,6 @@ public class jen1{
   public static void main(String args[]){
 	System.out.println("This is the jenkins practical");
 	System.out.println("Hello Atharva!!");
+	System.out.println("This is the update done in the file.");
   }
 }
